c5f36e59611bb567bc8648ea8901080b2f7b2490
KarelPeeters456 <karel@borndigital.co.nz>
2024-04-18 22:57:52 +1200

    removed takeaway test from experience
    So the takeaways section is hidden for that experience

--- js/content.js
+++ js/content.js
@@ -96,7 +96,7 @@ var content = {
                     ]
                 }
             ],
-            takeaways: "test"
+            takeaways: ""
         },
         {
             key: "soludtiondynamics",
